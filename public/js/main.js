5477e27c9b5ca0ea5ad82b379541616ca447013a
Francisco Garcia <belcboo@gmail.com>
2018-08-31 01:47:51 -0400

    Displayin user logged in. Modifying Handlebar.

--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 
   $.get("/api/userData").then(function(data) {
     console.log(data);
-    $("#navbarDropdownMenuLink").text("Logged in as: "data.email);
+    $("#navbarDropdownMenuLink").text("Logged in as: " + data.email);
   });
 
 });
