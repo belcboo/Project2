406cc401368e9fa7a1b16574cd3d805bfa276d26
Francisco <belcboo@gmail.com>
2018-09-01 03:06:53 -0400

    Rental > New : Form Completed. Time to check fields on model.

--- public/js/rental_new.js
+++ public/js/rental_new.js
@@ -10,8 +10,11 @@ $(document).ready(function() {
   var formPhone = $("#clientPhone");
   var clientsModal = $("#clientsModal");
   var submitDates = $("#submitDates");
-  var startDate = $("#startDate").val().trim();
-  var endDate = $("#endDate").val().trim();
+  var totalDays = $("#totalDays");
+  var grandTotal = $("#grandTotal");
+  var rowCounter = 0;
+  var orderTotal = 0;
+
 
 
   //Updates the title to match the actual page.
@@ -26,9 +29,18 @@ $(document).ready(function() {
   //Calculate days of rent
   submitDates.on('click', function(event){
     event.preventDefault();
-    var a = moment(startDate, 'YYYY/M/D');
-    var b = moment(endDate, 'YYYY/M/D');
+
+    var startDate = $("#startDate").val().trim();
+    var endDate = $("#endDate").val().trim();
+
+    var a = moment(startDate, 'YYYY/MM/DD');
+    var b = moment(endDate, 'YYYY/MM/DD');
     var diffDays = b.diff(a, 'days');
+    console.log(startDate, endDate, diffDays);
+    if(diffDays <= 0){
+      alert("Days can't be equals or lower than 0. Please check the dates.");
+    }
+    totalDays.val(diffDays);
   })
 
 
@@ -58,6 +70,42 @@ $(document).ready(function() {
 
   });
 
-  //On click evento to add products to the order.
-  $(document.body).on('')
+  //On Click event to add client to order's form.
+  $(document.body).on('click', "#productSelect", function(event){
+    event.preventDefault();
+
+
+    //The button has an id that coincides with the number of row.
+    var idBtn = $(this).attr("product-id");
+
+    //Gets the values of the colums.
+    var productQty = $("#inventoryModal #test"+idBtn).val().trim();
+    var product = document.getElementById("inventoryTbl").rows[idBtn].cells[1].innerHTML;
+    var price = document.getElementById("inventoryTbl").rows[idBtn].cells[2].innerHTML;
+    var total = totalDays.val() * price * productQty;
+
+    //Creating new row to display item selected.
+    var tbody = $("#tbody");
+    var tr = $("<tr>");
+    var td1 = $("<td>");
+    var td2 = $("<td>");
+    var td3 = $("<td>");
+    var td4 = $("<td>");
+
+    td1.append(productQty);
+    td2.append(product);
+    td2.attr("product_id", idBtn);
+    td3.append(price);
+    td4.append(total);
+    td4.attr("value",total);
+    tr.append(td1, td2, td3, td4);
+    tbody.append(tr);
+
+    // Updates Order Total
+    orderTotal += total;
+    grandTotal.text("ORDER TOTAL: USD$" + orderTotal);
+
+
+  });
+
 });
