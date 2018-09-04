c3810480c0facb93889eb71a199fa29ec2f56833
Jose De Las Salas <jld03e@gmail.com>
2018-09-03 23:49:51 -0400

    added search filters to inventory page, along with the search bar.

--- public/js/client.js
+++ public/js/client.js
@@ -1,3 +1,21 @@
+function search() {
+    var input, filter, table, tr, td, i;
+    input = document.getElementById("clientSearch");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("clientTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[2,4];
+      if (td) {
+        if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+  }
+
 $(document).ready(function() {
 
 //Updates the title to match the actual page.
@@ -8,26 +26,5 @@ $("#dashboardMenu").addClass('btn-outline-secondary');
 $("#rentalDrop").addClass('btn-outline-secondary');
 $("#clientDrop").addClass('btn-outline-danger');
 $("#inventoryDrop").addClass('btn-outline-secondary');
-
-
-// function search() {
-//   var input, filter, table, tr, td, i;
-//   input = document.getElementById("clientSearch");
-//   filter = input.value.toUpperCase();
-//   table = document.getElementById("clientTable");
-//   tr = table.getElementsByTagName("tr");
-//   for (i = 0; i < tr.length; i++) {
-//     td = tr[i].getElementsByTagName("td")[2];
-//     if (td) {
-//       if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-//         tr[i].style.display = "";
-//       } else {
-//         tr[i].style.display = "none";
-//       }
-//     }
-//   }
-// }
-
-// search();
 
 });
