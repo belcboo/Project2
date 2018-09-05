a946ac1921257a7a6ba576ca3cbecf6989cc652f
Francisco Garcia <belcboo@gmail.com>
2018-09-04 21:23:40 -0400

    Update to inventory list + updates to modals in rental page. Filters applied.

--- public/js/inventory.js
+++ public/js/inventory.js
@@ -5,7 +5,7 @@ function search() {
   table = document.getElementById("inventoryTable");
   tr = table.getElementsByTagName("tr");
   for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[1];
+    td = tr[i].getElementsByTagName("td")[0];
     if (td) {
       if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
         tr[i].style.display = "";
